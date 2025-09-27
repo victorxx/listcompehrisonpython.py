4bd3b0596fe81dbf3429b5d7a9c08bff910785ca
victorxx <victorsaib@gmail.com>
2025-09-27 20:22:51 -0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,15 @@
+
+
+
+
+numeros=[1,1,12]
+divisao=[numero / 2 for numero in numeros]
+multiplicacao=[numero * 2 for numero in numeros]
+soma=[numero + 2 for numero in numeros]
+potencia=[numero **2 for numero in numeros]
+print('potencia',potencia)
+print('soma',soma)
+print('multiplicação',multiplicacao)
+print('divisão',divisao)
+
+
